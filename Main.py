0786b6baa5a243eda613fcc22dcaba01879ca3f5
Kartikey <blake.276@yahoo.com>
2021-09-02 08:34:51 +0530

    Created Save Function and Exit Function, Fixed Date/Time Function to work properly

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import tkinter.messagebox as tmsg
 import tkinter.filedialog as tkfile
-import os, time
+import os, datetime
 
 class Notepad(Tk):
     
@@ -21,10 +21,10 @@ class Notepad(Tk):
         self.fileMenu = Menu(self.mainMenu, tearoff=0, relief=FLAT, activebackground='#82caff', activeforeground='black', bd=0)
         self.fileMenu.add_command(label='New', command=self.startNotepad, accelerator='Ctrl+N')
         self.fileMenu.add_command(label='Open...', accelerator='Ctrl+O', command=self.openFileNotepad)
-        self.fileMenu.add_command(label='Save', accelerator='Ctrl+S', command=self.saveFileNotepad)
-        self.fileMenu.add_command(label='Save As...')
+        self.fileMenu.add_command(label='Save', accelerator='Ctrl+S', command=self.saveFile)
+        self.fileMenu.add_command(label='Save As...', command=lambda : self.saveFile(saveAs=True))
         self.fileMenu.add_separator()
-        self.fileMenu.add_command(label='Exit', command=quit)
+        self.fileMenu.add_command(label='Exit', command=self.whenExit)
         self.mainMenu.add_cascade(label='File', menu=self.fileMenu)
 
     def addEditMenu(self):
@@ -38,7 +38,7 @@ class Notepad(Tk):
         self.editMenu.add_command(label='Delete', command=self.__delete, accelerator=' '*6+'Del')
         self.editMenu.add_separator()
         self.editMenu.add_command(label='Select All', command=lambda: self.textBox.tag_add(SEL, '1.0', END), accelerator='Ctrl+A')
-        self.editMenu.add_command(label='Time/Date', command=lambda: self.textBox.insert('insert', time.asctime(time.localtime())), accelerator=' '*8+'F5')
+        self.editMenu.add_command(label='Time/Date', command=self.__Datetime, accelerator=' '*8+'F5')
         self.mainMenu.add_cascade(label='Edit', menu=self.editMenu)
 
     def addFormatMenu(self):
@@ -111,6 +111,10 @@ class Notepad(Tk):
         except:
             pass
 
+    def __Datetime(self):
+        self.__delete()
+        self.textBox.insert('insert', datetime.datetime.now().strftime('%H:%M %d-%m-%Y'))
+
     def wordWrap_(self):
         if self.wordWrap.get():
             self.textBox.config(wrap=WORD)
@@ -128,7 +132,7 @@ class Notepad(Tk):
     
     def statusBarUpdate(self):
         pos = self.textBox.index(INSERT).split('.')
-        pos = f'Ln {pos[0]}, Col {pos[1]}'+' '*30
+        pos = f'Ln {pos[0]}, Col {int(pos[1])+1}'+' '*30
         self.statusBar.config(text=pos)
         self.after(2, self.statusBarUpdate)
 
@@ -145,17 +149,17 @@ class Notepad(Tk):
 
     def startNotepad(self):
         if self.filename:
-            with open(self.title()[:-1]) as f:
+            with open(self.filename, 'r') as f:
                 txt = f.read()
             if txt+'\n' == self.textBox.get("1.0", END):
                 self.newNote()
             else:
-                pass
+                self.actionForFile(self.newNote)
         else:
             if self.textBox.get("1.0", END) == '\n':
                 self.newNote()
             else:
-                pass
+                self.actionForFile(self.newNote)
 
     def openNote(self):
         try:
@@ -165,41 +169,66 @@ class Notepad(Tk):
             self.textBox.delete("1.0", END)
             self.textBox.insert("1.0", txt)
             self.title(f'{filename} - Notepad')
+            self.filename = filename
         except:
             pass
 
     def openFileNotepad(self):
         if self.filename:
-            with open(self.title()[:-10], 'r') as f:
+            with open(self.filename, 'r') as f:
                 txt = f.read()
             if txt+'\n' == self.textBox.get("1.0", END):
                 self.openNote()
             else:
-                pass
+                self.actionForFile(self.openNote)
         else:
             if self.textBox.get("1.0", END) == '\n':
                 self.openNote()
             else:
-                pass
+                self.actionForFile(self.openNote)
 
-    def saveFile(self):
+    def saveFile(self, saveAs=False):
+        if self.filename and not saveAs:
+            with open(self.filename, 'w') as f:
+                f.write(self.textBox.get("1.0", END)[:-1])
+            return True
         try:
-            filename = tkfile.askopenfilename()
-            if not filename:
+            filename = tkfile.asksaveasfilename()
+            if filename:
                 with open(filename, 'w') as f:
                     f.write(self.textBox.get("1.0", END)[:-1])
                 self.title(f'{filename} - Notepad')
+                self.filename = filename
                 return True
             else:
                 return False
         except:
             return False
 
+    def actionForFile(self, funcToPerform):
+        dialog = tmsg.askyesnocancel('Notepad', 'Do you want to save this file ?')
+        if dialog == None:
+            pass
+        elif dialog == False:
+            funcToPerform()
+        else:
+            saved = self.saveFile()
+            if saved:
+                funcToPerform()
+
+    def whenExit(self):
+        if self.filename:
+            with open(self.filename, 'r') as f:
+                txt = f.read()
+            if txt == self.textBox.get("1.0", END)[:-1]:
+                self.quit()
+            self.actionForFile(self.quit)
+        else:
+            if self.textBox.get("1.0", END) == '\n':
+                self.quit()
+            self.quit()
+
         
-
-    def saveFileNotepad(self):
-        pass
-
 
 if __name__ == '__main__':
     notepad = Notepad()
